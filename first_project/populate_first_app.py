3a84cb6b790590c048df02d141a44d6e301418c6
Uday surya <udaysurya1597@gmail.com>
2026-01-21 20:11:45 +0530

    populated date with faker

---
+++ first_project/populate_first_app.py
@@ -0,0 +1,43 @@
+import os
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'first_project.settings')
+
+import django
+django.setup()
+
+import random
+from first_app.models import AccessRecord, Webpage, Topic
+from faker import Faker
+
+fakegen = Faker()
+topics = ['Search', 'Social', 'Marketplace', 'News', 'Games']
+
+
+def add_topic():
+    t = Topic.objects.get_or_create(top_name=random.choice(topics))[0]
+    return t
+
+
+def populate(N=5):
+    for _ in range(N):
+        top = add_topic()
+
+        fake_url = fakegen.url()
+        fake_date = fakegen.date()
+        fake_name = fakegen.company()
+
+        webpg = Webpage.objects.get_or_create(
+            topic=top,
+            url=fake_url,
+            name=fake_name
+        )[0]
+
+        AccessRecord.objects.get_or_create(
+            name=webpg,
+            date=fake_date
+        )
+
+
+if __name__ == '__main__':
+    print("Populating script!")
+    populate(20)
+    print("Populating complete.")
